4037ffdbea4938be4880bfba0621832e63b566a7
Xiaoyun Zhi <Xiaoyun@192.168.0.103>
2018-02-20 10:59:33 +0800

    before add functions to Category

---
+++ Todoey/CategoryViewController.swift
@@ -0,0 +1,32 @@
+//
+//  CategoryViewController.swift
+//  Todoey
+//
+//  Created by Xiaoyun Zhi on 2/20/18.
+//  Copyright © 2018 Xiaoyun Zhi. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CategoryViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+    }
+    
+    //MARK: - TableView Datasource Methods
+    
+    
+    //MARK: - TableView Delegate Methods
+    
+    
+    //MARK: - Data Manipulation Methods
+    
+}
